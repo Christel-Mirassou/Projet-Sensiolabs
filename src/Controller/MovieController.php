2df390403a9bee2320bcdec9fde460012233d4a9
Christel <72789056+Christel-Mirassou@users.noreply.github.com>
2022-02-08 15:02:37 +0100

    Doctrine

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\OmdbApi;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MovieController extends AbstractController
 {
+    private OmdbApi $omdbApi;
+
+    public function __construct()
+    {
+        $this->omdbApi = new OmdbApi(HttpClient::create() ,'28c5b7b1', 'https://www.omdbapi.com');
+        
+    }
+
     #[Route('/movie', name: 'movie')]
     public function index(): Response
     {
@@ -20,18 +31,21 @@ class MovieController extends AbstractController
     }
 
     #[Route('/movie/latest', name: 'movie_latest')]
-    public function latest(): Response
+    public function latest(MovieRepository $movieRepository): Response
     {
-        return $this->render('movie/latest.html.twig');
+        $movies = $movieRepository->findBy([],['id' => 'DESC']);
+
+        return $this->render('movie/latest.html.twig', [
+            'movies' => $movies,
+        ]);
     }
 
     #[Route('/movie/search', name: 'movie_search')]
     public function search(Request $request): Response
     {
         $keyword = $request->query->get('keyword', 'Sun');
-        $omdbApi = new OmdbApi(HttpClient::create() ,'28c5b7b1', 'https://www.omdbapi.com');
-        $movies = $omdbApi->requestAllBySearch($keyword);
-        dump($omdbApi, $movies);
+        $movies = $this->omdbApi->requestAllBySearch($keyword);
+        dump($this->omdbApi, $movies);
 
         return $this->render('movie/search.html.twig', [
             'movies' => $movies,
@@ -40,10 +54,28 @@ class MovieController extends AbstractController
     }
 
     #[Route('/movie/{id}', name: 'movie_show', requirements: ['id' => "\d+"])]
-    public function show(int $id): Response
+    public function show(int $id, MovieRepository $movieRepository): Response
     {
+        $movie = $movieRepository->findOneById($id);
+
         return $this->render('movie/show.html.twig', [
             'id' => $id,
+            'movie'=> $movie,
         ]);
     }
+    
+    #[Route('/movie/{imdbId}/import', name: 'movie_import')]
+    public function import($imdbId, EntityManagerInterface $manager): Response
+    {
+        $movieData = $this->omdbApi->requestOneById($imdbId);
+        $movie = Movie::fromApi($movieData);
+        // dump($movie);
+        // die;
+        $manager->persist($movie);
+        $manager->flush();
+
+        return $this->redirectToRoute('movie_latest');
+    }
+
+
 }
