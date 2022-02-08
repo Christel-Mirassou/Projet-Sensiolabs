ee6bdbddfef1e66515ef3270a25ed0f7b3c1784f
Christel <72789056+Christel-Mirassou@users.noreply.github.com>
2022-02-08 11:42:06 +0100

    Fin des forms

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     #[Route('/register', name: 'register')]
-    public function register(): Response
+    public function register(Request $request): Response
     {
         $form = $this->createForm(UserType::class);
 
+        // dump($form->getData());
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            dump($form->getData());
+        }
         return $this->render('user/register.html.twig', [
             'form' => $form->createView(),
             
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserType extends AbstractType
 {
@@ -24,11 +25,14 @@ class UserType extends AbstractType
             ->add('password', Type\RepeatedType::class, [
                 'type' => Type\PasswordType::class,
                 'first_options'=>['label' => 'Password'], 
-                'second_options'=>['label' => 'Cofirnm Password'] 
+                'second_options'=>['label' => 'Confirm Password'] 
             ])
             ->add('terms', Type\CheckboxType::class, [
+                'constraints' => [new Assert\IsTrue(null, 'Les CGU doivent être acceptées')],   //impose une validation côté serveur qui est plus puissante qu'un simple required->true qui n'est qu'une validation html
                 'label' => 'Click here to indicate that you have read and agree to the terms presented in the Terms and Conditions agreement',
-                'help' => 'Your email and information are used to allow you to sign in securely and access your data. SensioTV records certain usage data for security, support and reporting purposes.'
+                'help' => 'Your email and information are used to allow you to sign in securely and access your data. SensioTV records certain usage data for security, support and reporting purposes.',
+                'mapped' => false,  //ce champ ne fait pas partie de l'entité User
+
             ])
             ->add('save', Type\SubmitType::class, [
                 'label' => 'Create your SensioTV account'
